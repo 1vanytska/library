a9cbe03d1d728201d1cdb151c6cb2652ffdbb4bf
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2025-04-05 15:30:26 +0300

    Lab 3 - Remove the excess, add PostgreSQL

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'library.db')}"
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "0987")
+DB_NAME = os.getenv("POSTGRES_DB", "library_db")
+DB_HOST = os.getenv("POSTGRES_HOST", "db")
+DB_PORT = "5432"
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
